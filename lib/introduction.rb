eb99527d363d5a791249e21cff463fcfbc87d2a6
A Glazer <avigayilglazer@gmail.com>
2018-12-23 18:43:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
 def introduction (name)
   puts "Hi, my name is #{name}."
 end
+
+def introduction_with_language("Avigayil", "Ruby.rb")
+  puts "Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
